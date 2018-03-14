d40a212d6d04333588f4f7e9fcc104c4895d8c13
Davide Panettieri <davidepanettieri@cam-11.local>
2018-03-14 14:28:30 +0100

    geocode : label con città  della posizione corrente

--- map/ViewController.swift
+++ map/ViewController.swift
@@ -14,22 +14,33 @@ import MapKit
 
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    @IBOutlet weak var indirizzoLabel: UILabel!
     @IBOutlet weak var mappa: MKMapView!
     let locationManag = LocationManager.shared
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 200, 200)
         mappa.setRegion(coordinateRegion, animated: true)
-        var location = userLocation
+        let location = userLocation
+
         print(location.coordinate)
+        
+       
+       
+
+    
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
        mappa.delegate = self
         mappa.mapType = MKMapType.hybrid
         mappa.showsUserLocation = true
-        
-       
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(locationManag.location!) { (placemarks, error) in
+            
+            self.handlerIndirizzo(withPlacemarks: placemarks, error: error)
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -37,6 +48,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func handlerIndirizzo(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+
+        if let error = error {
+            
+            indirizzoLabel.text = "Indirizzo non disponibile"
+            
+        } else {
+            if let placemarks = placemarks, let placemark = placemarks.first {
+                indirizzoLabel.text = "\(placemark.locality!), \(placemark.country!)"
+            } else {
+                indirizzoLabel.text = "Indirizzo non disponibile "
+            }
+        }
     }
     
 
